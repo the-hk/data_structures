486cfed45e291b5fd3753645668483ad1b1c2523
the-hk <conjointchill@gmail.com>
2022-06-25 20:58:30 +0300

    bst.cpp and .h file was occured. there is add functionwas added butit is not completed

--- binarySearchTree/bst.cpp
+++ binarySearchTree/bst.cpp
@@ -1,1 +1,52 @@
 #include "bst.h"
+
+bstCls::bstCls()
+{
+    std::cout<<"bstCls was created"<<std::endl;
+}
+
+bstCls::~bstCls()
+{
+    std::cout<<"bstCls was destroyed"<<std::endl;
+}
+
+int bstCls::add_tree(int newValue)
+{
+    if(this->bst_root == nullptr)
+    {
+        /* create a root node for tree */
+        this->bst_root = new bstStc;
+        this->bst_root->val = newValue;
+        return 0;
+    }
+
+    bstStc *iter = this->bst_root;
+    if(newValue > this->bst_root->val)
+    {
+        /* right node */  
+        iter = iter->right;
+        add_tree()
+    }
+
+    if(newValue < this->bst_root->val)
+    {
+        /* left node */
+        iter = iter->left;
+
+    }
+}
+
+int bstCls::remove_tree()
+{
+
+}
+
+int bstCls::print_tree()
+{
+
+}
+
+int bstCls::search_tree()
+{
+
+}
--- binarySearchTree/bst.h
+++ binarySearchTree/bst.h
@@ -1,2 +1,24 @@
 #include <iostream>
 
+struct bstStc
+{
+    int val;
+    bstStc *left;
+    bstStc *right;
+};
+
+class bstCls
+{
+    public:
+    bstCls();
+    ~bstCls();
+    int add_tree(int newValue);
+    int remove_tree();
+    int print_tree();
+    int search_tree();
+
+    private:
+    bstStc *bst_root = nullptr;
+    bstStc *bst_iter = nullptr;
+
+};
